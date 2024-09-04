3c788924717cc54eee2b3a63941c991842fb627c
Mykhailo Kan <mykhailo.kan@giss.pl>
2024-09-04 10:07:50 +0200

    rotate array

--- src/easy/rotate_array.rs
+++ src/easy/rotate_array.rs
@@ -1,19 +1,28 @@
 // https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/646/
 #[allow(dead_code)]
 fn solve(nums: &mut Vec<i32>, k: i32) {
-    let mut next_hop = nums[0];
-    let mut odd_case = 0;
-    for i in 1..nums.len()+1 {
-        let offset = (k as usize*i)%nums.len() + odd_case;
-        
-        let tmp = nums[offset];
-        nums[offset] = next_hop;
-        next_hop = tmp; 
-        if offset == 0 && k % 2 == 0 && nums.len() > 1 {
-            next_hop = nums[1];
-            odd_case = 1;
+    let tmp = nums.clone();
+    for i in 0..nums.len() {
+        let mut idx = (i as i32 - k) % nums.len() as i32;
+        if idx < 0 {
+            idx = (nums.len() as i32 + idx) % nums.len() as i32;
         }
+        nums[i] = tmp[idx as usize];
     }
+    // // in place (not working)
+    // let mut next_hop = nums[0];
+    // let mut odd_case = 0;
+    // for i in 1..nums.len()+1 {
+    //     let offset = (k as usize*i)%nums.len() + odd_case;
+        
+    //     let tmp = nums[offset];
+    //     nums[offset] = next_hop;
+    //     next_hop = tmp; 
+    //     if offset == 0 && k % 2 == 0 && nums.len() > 1 {
+    //         next_hop = nums[1];
+    //         odd_case = 1;
+    //     }
+    // }
 }
 
 #[allow(dead_code)]
@@ -25,7 +34,7 @@ pub fn test() {
     // solve(&mut nums, 2);
     // println!("{:?}", nums);
     
-    let mut nums = vec![1,2,3,4,5,6];
+    let mut nums = vec![1,2,3,4,5,6,7];
     solve(&mut nums, 3);
     println!("{:?}", nums);
     // let mut nums = vec![1];
